7088e80b055b622c53105c89101f715b836e7b04
Manabu ISHII <manabu.ishii.q@gmail.com>
2018-04-13 00:10:06 +0900

    AGC 008 A

--- atcoder/agc008_a/agc008_a.py
+++ atcoder/agc008_a/agc008_a.py
@@ -5,11 +5,22 @@
 x,y = [int(i) for i in input().split()]
 
 res = 0
-if x < 0:
-    res = res + 1
-if y < 0:
-    res = res + 1
 
-res = res + (abs(y)-abs(x))
+
+if abs(y) > abs(x):
+    # |x| < |y|
+    if x < 0:
+        res = res + 1
+    if y < 0:
+        res = res + 1
+    res = res + (abs(y)-abs(x))
+else :
+    # x > y
+    if x > 0:
+        res = res + 1
+    if y > 0:
+        res = res + 1
+    res = res + (abs(x)-abs(y))
+
 
 print(res)
